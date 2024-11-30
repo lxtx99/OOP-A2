a018ba0e2acf86c3b9b4f4f97c11bb78d9607c58
lxtx99 <2946697834@qq.com>
2024-11-30 15:09:27 +0800

    print partThree

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,16 +1,65 @@
-public class AssignmentTwo { 
-    public static void main(String[] args) { 
-    } 
-    public void partThree(){ 
-    } 
-    public void partFourA(){ 
-    } 
-    public void partFourB(){ 
-    } 
-    public void partFive(){ 
-    } 
-    public void partSix(){ 
-    } 
-    public void partSeven(){ 
-    } 
+public class AssignmentTwo {
+    
+    public static void main(String[] args) {
+        // 在 main 方法中调用 partThree 方法来测试
+        AssignmentTwo assignment = new AssignmentTwo();
+        assignment.partThree();  // 调用 partThree 执行任务
     }
+
+    // partThree 方法：实现创建 Ride，游客 Visitor，操作队列等逻辑
+    public void partThree() {
+        // 创建一个 Ride 对象
+        Employee rideOperator = new Employee("John Doe", 35, "Male", "123-456-7890", "E001", "Ride Operator");
+        Ride rollerCoaster = new Ride("Roller Coaster", "Thrill", true, rideOperator);
+
+        // 创建一些 Visitor 对象
+        Visitor visitor1 = new Visitor("Jack", 25, "Male", "111-222-3333", "Adult", false);
+        Visitor visitor2 = new Visitor("Sharon", 30, "Female", "222-333-4444", "Adult", true);
+        Visitor visitor3 = new Visitor("Benny", 22, "Male", "333-444-5555", "Student", false);
+        Visitor visitor4 = new Visitor("Leo", 28, "Male", "444-555-6666", "Adult", true);
+        Visitor visitor5 = new Visitor("Nehemia", 35, "Female", "555-666-7777", "Senior", false);
+
+        // 将游客添加到队列
+        rollerCoaster.addVisitorToQueue(visitor1);
+        rollerCoaster.addVisitorToQueue(visitor2);
+        rollerCoaster.addVisitorToQueue(visitor3);
+        rollerCoaster.addVisitorToQueue(visitor4);
+        rollerCoaster.addVisitorToQueue(visitor5);
+
+        // 打印当前队列中的游客
+        System.out.println("Current Visitor Queue:");
+        rollerCoaster.printQueue();
+
+        // 从队列中移除一个游客
+        rollerCoaster.removeVisitorFromQueue(visitor3);  // 移除 Benny
+
+        // 再次打印队列中的游客
+        System.out.println("\nUpdated Visitor Queue (after removal):");
+        rollerCoaster.printQueue();
+    }
+
+    // partFourA 方法：你可以在此方法中添加相应功能
+    public void partFourA() {
+        // 你可以根据需要在此方法中添加具体实现
+    }
+
+    // partFourB 方法：你可以在此方法中添加相应功能
+    public void partFourB() {
+        // 你可以根据需要在此方法中添加具体实现
+    }
+
+    // partFive 方法：你可以在此方法中添加相应功能
+    public void partFive() {
+        // 你可以根据需要在此方法中添加具体实现
+    }
+
+    // partSix 方法：你可以在此方法中添加相应功能
+    public void partSix() {
+        // 你可以根据需要在此方法中添加具体实现
+    }
+
+    // partSeven 方法：你可以在此方法中添加相应功能
+    public void partSeven() {
+        // 你可以根据需要在此方法中添加具体实现
+    }
+}
